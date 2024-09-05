34a42560a5488ae4a6f45d95185cdcc14221747d
pavlobarabah <pavlobarabah@gmail.com>
2024-09-05 12:47:07 +0300

    added environment variables

--- src/main/java/com/crm_for_bot/controller/UserController.java
+++ src/main/java/com/crm_for_bot/controller/UserController.java
@@ -6,13 +6,10 @@ import com.crm_for_bot.entity.User;
 import com.crm_for_bot.exception.RecourseNotFoundException;
 import com.crm_for_bot.mapper.UserDtoMapper;
 import com.crm_for_bot.service.UserService;
-import com.crm_for_bot.service.UserServiceImpl;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
